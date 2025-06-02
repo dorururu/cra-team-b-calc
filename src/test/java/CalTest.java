2a04881e32898fe6cdbf7810ea2dfb8057490e91
ingyusuh <gitfdj.com>
2025-06-02 15:30:08 +0900

    getSum function UT 추가

--- src/test/java/CalTest.java
+++ src/test/java/CalTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalTest {
 
     @Test
-    void getSum() {
+    void getSum_both_positive() {
         Cal cal = new Cal();
         int input1 = 3;
         int input2 = 4;
@@ -15,4 +15,30 @@ class CalTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void getSum_both_negative() {
+        Cal cal = new Cal();
+        int input1 = -3;
+        int input2 = -4;
+        int expected = -7;
+
+        int actual = cal.getSum(input1, input2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getSum_both_zero() {
+        Cal cal = new Cal();
+        int input1 = 0;
+        int input2 = 0;
+        int expected = 0;
+
+        int actual = cal.getSum(input1, input2);
+
+        assertEquals(expected, actual);
+    }
+
+
 }
