d0092ee0301a8bc5a5a1ca54e31c969e30bbae46
magicers <magicers123@gmail.com>
2025-06-02 15:27:05 +0900

    getDivide

--- src/main/java/Cal.java
+++ src/main/java/Cal.java
@@ -1,3 +1,7 @@
 public class Cal {
-    // 수정
+    int getDivide(int a, int b){
+        if(b==0)
+            throw new RuntimeException("0으로 나눌수 없습니다");
+        return a/b;
+    }
 }
--- src/test/java/CalTest.java
+++ src/test/java/CalTest.java
@@ -1,5 +1,16 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalTest {
 
+    @Test
+    void getDivide() {
+        Cal c = new Cal();
+        int expected = 3;
+
+        int actual = c.getDivide(6,2);
+
+        assertEquals(expected, actual);
+    }
 }
