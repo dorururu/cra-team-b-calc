2c583914187362bd2a249fdfe58dc23dbb913157
hyehye <hye1.jeong@samsung.com>
2025-06-02 15:16:30 +0900

    곱하기 함수 테스트 케이스 추가

--- src/main/java/Cal.java
+++ src/main/java/Cal.java
@@ -1,3 +1,7 @@
 public class Cal {
     // 수정
+
+    int getGop(int a, int b) {
+        return a * b;
+    }
 }
--- src/test/java/CalTest.java
+++ src/test/java/CalTest.java
@@ -1,5 +1,20 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalTest {
 
+    @Test
+    @DisplayName("곱하기 테스트")
+    void getGop() {
+        int a = 10;
+        int b = -5;
+        int expected = -50;
+
+        Cal cal = new Cal();
+        int actual = cal.getGop(a, b);
+
+        assertEquals(expected, actual);
+    }
 }
