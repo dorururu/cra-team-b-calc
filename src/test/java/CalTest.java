779efb4159f57497838ac2b6a5bc489e1d624d98
happyskygang <happysky.gang@gmail.com>
2025-06-02 15:16:53 +0900

    Add getZegop TC

--- src/test/java/CalTest.java
+++ src/test/java/CalTest.java
@@ -1,5 +1,10 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalTest {
-
+    @Test
+    public void getZegopTest(){ 
+        int testVal = 10;
+        int returnV = getZegop(testVal);
+        assertEqual(100, returnV);
+    }
 }
