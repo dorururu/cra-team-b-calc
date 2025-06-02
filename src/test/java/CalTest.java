b7cdfc5f47da0e28d418ce26c6f6382895cff8fe
dudalss <myemail@emap.com>
2025-06-02 15:17:20 +0900

    getsumsum #2

--- src/test/java/CalTest.java
+++ src/test/java/CalTest.java
@@ -1,5 +1,19 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalTest {
 
+    @Test
+    void getSumSum() {
+        // arrange
+        Cal cal = new Cal();
+        int expected = 10;
+
+        // act
+        int actual = cal.getSumSum(10, 3, 2);
+
+        // assert
+        assertEquals(expected, actual);
+    }
 }
