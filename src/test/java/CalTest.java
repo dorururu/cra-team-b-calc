2d194c3ca6f34b0d96a36d8979eb2e6835ce4b03
ingyusuh <gitfdj.com>
2025-06-02 15:12:47 +0900

    add getSum Function

--- src/test/java/CalTest.java
+++ src/test/java/CalTest.java
@@ -1,5 +1,18 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalTest {
 
+    @Test
+    void getSum() {
+        Cal cal = new Cal();
+        int input1 = 3;
+        int input2 = 4;
+        int expected = 7;
+
+        int actual = cal.getSum(input1, input2);
+
+        assertEquals(expected, actual);
+    }
 }
